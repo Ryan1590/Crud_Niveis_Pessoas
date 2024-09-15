e69d15dd654466117dedbc00edac7bd0787d30de
Ryan Rodrigues <ryanneno1590@gmail.com>
2024-09-15 16:44:08 -0300

    configurações basicas e migrations

---
+++ database/migrations/2024_09_15_193545_create_desenvolvedores_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('desenvolvedores', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('nivel_id')->constrained('niveis'); // Chave estrangeira para a tabela 'niveis'
+            $table->string('nome');
+            $table->string('sexo');
+            $table->date('data_nasciento');
+            $table->string('hobby')->nullable();  // Campo 'hobby' do tipo varchar, pode ser nulo
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('desenvolvedores');
+    }
+};
